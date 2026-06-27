18ee74ce442260c5a7dc380ca4117cad0633067d
jonathankabor <kaborejbaime@gmail.com>
2026-06-27 18:17:51 +0200

    ScrollView et PageLayout

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from tkinter.ttk import Button
+#from tkinter.ttk import Button
+from kivy.uix.button import Button
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.metrics import dp
 
 class MainWidget(Widget):
     pass
 
-#class GridLayoutExemple(GridLayout):
-#   pass
+class GridLayoutExemple(GridLayout):
+    pass
+
+class StackLayoutExemple(StackLayout):
+   def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = "lr-bt"
+        for i in range(0, 100):
+            b = Button(text=str(i + 1), size_hint=(None, None), size=(dp(100), dp(100)))
+            self.add_widget(b)
 
 class AnchorLayoutExemple(AnchorLayout):
     pass
